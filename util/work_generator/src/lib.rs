81302cf927c8d188301063e6eabcf7b615f15eec
Kevin Zhao <kzhao0986@gmail.com>
2024-09-28 14:27:16 -0700

    stop everything when time expires

--- util/work_generator/src/lib.rs
+++ util/work_generator/src/lib.rs
@@ -8,8 +8,11 @@ use rand_distr::{Distribution, Exp};
 pub use request::*;
 use reqwest::{Client, Url};
 use serde::{Deserialize, Serialize};
+use tokio::sync::mpsc;
 use tokio::task;
 use tokio::time::{Duration, Instant};
+use tokio_stream::wrappers::UnboundedReceiverStream;
+use tokio_stream::StreamExt;
 pub use units::*;
 
 #[builder]
@@ -55,70 +58,74 @@ const HOST: &str = "onlineboutique.serviceweaver.dev";
 
 async fn run_one_connection(workload: ConnWorkload) -> Result<Vec<Record>, Error> {
     let client = Client::new();
-    let mut handles = Vec::new();
     let mut now = Instant::now();
     let end = now + workload.duration;
+    let (tx, rx) = mpsc::unbounded_channel();
+    let mut stream = UnboundedReceiverStream::new(rx);
     let mut rng = StdRng::from_entropy();
-    while now < end {
-        // Wait until the next RPC time.
-        let delta = workload.deltas.sample(&mut rng).round() as u64;
-        let timer = async_timer::new_timer(Duration::from_nanos(delta));
-        timer.await;
+    tokio::spawn(async move {
+        while now < end {
+            // Wait until the next RPC time.
+            let delta = workload.deltas.sample(&mut rng).round() as u64;
+            let timer = async_timer::new_timer(Duration::from_nanos(delta));
+            timer.await;
 
-        // Gather request data.
-        let url = workload.url.clone();
-        let client = client.clone();
-        let send_fut = match workload.mix.sample(&mut rng) {
-            RequestData::Home(form) => client.get(url).header("Host", HOST).form(&form).send(),
-            RequestData::Product(not_a_form) => {
-                let url = format!("{url}{}", not_a_form.id);
-                client.get(url).header("Host", HOST).send()
-            }
-            RequestData::ViewCart(form) => {
-                let url = format!("{url}cart");
-                client.get(url).header("Host", HOST).form(&form).send()
-            }
-            RequestData::AddToCart(form) => {
-                let url = format!("{url}cart");
-                client.post(url).header("Host", HOST).form(&form).send()
-            }
-            RequestData::EmptyCart(form) => {
-                let url = format!("{url}cart/empty");
-                client
-                    .post(url)
-                    .header("Host", HOST)
-                    .header("Content-Length", "0") // required
-                    .form(&form)
-                    .send()
-            }
-            RequestData::SetCurrency(form) => {
-                let url = format!("{url}setCurrency");
-                client.post(url).header("Host", HOST).form(&form).send()
-            }
-            RequestData::Logout(form) => {
-                let url = format!("{url}logout");
-                client.get(url).header("Host", HOST).form(&form).send()
-            }
-            RequestData::PlaceOrder(form) => {
-                let url = format!("{url}cart/checkout");
-                client.post(url).header("Host", HOST).form(&form).send()
-            }
-        };
+            // Gather request data.
+            let url = workload.url.clone();
+            let client = client.clone();
+            let send_fut = match workload.mix.sample(&mut rng) {
+                RequestData::Home(form) => client.get(url).header("Host", HOST).form(&form).send(),
+                RequestData::Product(not_a_form) => {
+                    let url = format!("{url}{}", not_a_form.id);
+                    client.get(url).header("Host", HOST).send()
+                }
+                RequestData::ViewCart(form) => {
+                    let url = format!("{url}cart");
+                    client.get(url).header("Host", HOST).form(&form).send()
+                }
+                RequestData::AddToCart(form) => {
+                    let url = format!("{url}cart");
+                    client.post(url).header("Host", HOST).form(&form).send()
+                }
+                RequestData::EmptyCart(form) => {
+                    let url = format!("{url}cart/empty");
+                    client
+                        .post(url)
+                        .header("Host", HOST)
+                        .header("Content-Length", "0") // required
+                        .form(&form)
+                        .send()
+                }
+                RequestData::SetCurrency(form) => {
+                    let url = format!("{url}setCurrency");
+                    client.post(url).header("Host", HOST).form(&form).send()
+                }
+                RequestData::Logout(form) => {
+                    let url = format!("{url}logout");
+                    client.get(url).header("Host", HOST).form(&form).send()
+                }
+                RequestData::PlaceOrder(form) => {
+                    let url = format!("{url}cart/checkout");
+                    client.post(url).header("Host", HOST).form(&form).send()
+                }
+            };
 
-        // Send the next request.
-        let handle = task::spawn(async move {
-            let now = Instant::now();
-            let _ = send_fut.await?.error_for_status()?;
-            let elapsed = now.elapsed();
-            Result::<_, Error>::Ok(elapsed)
-        });
-        handles.push(handle);
-        now = Instant::now();
-    }
+            // Send the next request.
+            let tx = tx.clone();
+            task::spawn(async move {
+                let now = Instant::now();
+                let _ = send_fut.await?.error_for_status()?;
+                let elapsed = now.elapsed();
+                let latency = Microsecs::new(elapsed.as_micros());
+                let _ = tx.send(Token::Latency(latency));
+                Result::<_, Error>::Ok(())
+            });
+            now = Instant::now();
+        }
+        tx.send(Token::Stop).unwrap();
+    });
     let mut records = Vec::new();
-    for handle in handles {
-        let elapsed = handle.await??;
-        let latency = Microsecs::new(elapsed.as_micros());
+    while let Some(Token::Latency(latency)) = stream.next().await {
         records.push(Record { latency })
     }
     Ok(records)
@@ -127,6 +134,12 @@ async fn run_one_connection(workload: ConnWorkload) -> Result<Vec<Record>, Error
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Record {
     pub latency: Microsecs,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Token {
+    Latency(Microsecs),
+    Stop,
 }
 
 #[derive(Debug, thiserror::Error)]
